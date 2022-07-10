95eda0fd2b609e10b279a93e710e7247d7bece96
afisher93 <106286620+afisher93@users.noreply.github.com>
2022-07-10 15:15:31 -0500

    comeplete

--- app.js
+++ app.js
@@ -7,13 +7,37 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
-
+let a = 20;
+let b = 4;
+let add = a + b;
+let substract = a - b;
+let multiply = a * b;
+let divide = a / b;
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+console.log(val == str);
+console.log(val === str);
+console.log(!isPresent);
+console.log("eleven" == str2 && val >= str);
+console.log(!isPresent || isPresent);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
